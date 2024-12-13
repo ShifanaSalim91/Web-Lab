dd0b431bb89474ea3aa61a095256b95ba3c6295d
ShifanaSalim91 <shifanashif91@gmail.com>
2024-12-13 09:54:19 +0530

    student registration

---
+++ homepage.php
@@ -0,0 +1,14 @@
+<html>
+	<body>
+	<center>
+	<a href="login2.php">Home</a><br>
+	<a href="studentreg.php">Add Student details</a><br>
+	<a href="viewstudent.php">View All Students</a><br>
+	<a href="search.php">Search a Student with a Student Id</a><br>
+
+	<a href="deletestudent.php">Delete a Student </a><br>
+	<a href="UPDATE1.php">Update Student details</a><br>
+	<a href="logout.php">Logout</a><br>
+	</center>
+	</body>
+</html>
